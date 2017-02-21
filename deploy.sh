847a4395ca64f9f0dd63f02b41b0f0d7809e6d3e
pshen <patrick.lc.shen@gmail.com>
2017-02-21 14:22:13 -0800

    (feat) change 49016 to 5000 for circle CI

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ echo "pulling latest version of the code"
 ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull nanongage/ngage-db:latest'
 
 echo "starting the new version"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --name ngage-db -p 49016:5000 nanongage/ngage-db:latest'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --name ngage-db -p 5000:5000 nanongage/ngage-db:latest'
 
 echo "success!"
 
